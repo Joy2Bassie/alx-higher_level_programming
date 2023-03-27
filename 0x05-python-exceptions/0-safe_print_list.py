82b59abeede0b1d3d4c842a1c13a150e0aeba9ab
Joy <joybassie909@gmail.com>
2023-03-27 09:59:08 -0700

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3 
-  def safe_print_list(my_list=[], x=0):
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
 
     """Print x elememts of a list.
 
@@ -34,4 +34,3 @@
     print("")
 
     return (ret)
-
